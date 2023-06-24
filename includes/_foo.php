7d925881074f52892b474a86f924dfd0ff589b2f
Luiz Bills <luizbills@pm.me>
2023-06-24 13:23:42 -0300

    Update _foo.php

--- includes/_foo.php
+++ includes/_foo.php
@@ -1,5 +1,5 @@
 <?php
 
-/* Files with underscore at begining are not included */
+/* Files with underscore or dot at begining will not be executed */
 
 err_log( pathinfo( __FILE__, PATHINFO_BASENAME ) . ' included' );
