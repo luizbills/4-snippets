6d7a8b3dd3ed362498ff52bbfafd16387fe2f79b
Luiz Bills <luizbills@pm.me>
2023-07-03 08:24:27 -0300

    Create 00-autoload.php

---
+++ includes/.demos/00-autoload.php
@@ -0,0 +1,9 @@
+<?php
+
+/* Load the Composer autoload */
+$autoload = Plugin_4_Snippets::DIR . '/vendor/autoload.php';
+if ( file_exists( $autoload ) {
+    include $autoload;
+} else {
+    wp_die( "<code>$autoload</code> not found!" );
+}
