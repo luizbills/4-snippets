d750f37c1542900b3d9ea254bd9dd3c787a2f284
Luiz Bills <luizbills@pm.me>
2023-07-03 08:26:00 -0300

    Update 00-autoload.php

--- includes/.demos/00-autoload.php
+++ includes/.demos/00-autoload.php
@@ -3,7 +3,7 @@
 /* Load the Composer autoload */
 $autoload = Plugin_4_Snippets::DIR . '/vendor/autoload.php';
 if ( file_exists( $autoload ) {
-    include $autoload;
+	include $autoload;
 } else {
-    wp_die( "<code>$autoload</code> not found!" );
+	wp_die( "<code>$autoload</code> not found!" );
 }
