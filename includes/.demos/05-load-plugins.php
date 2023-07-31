2b18e7eab175bc602c1afaefb4d66006475f09ab
Luiz Bills <luizbills@pm.me>
2023-07-31 08:42:56 -0300

    improve snippet to load plugins

--- includes/.demos/05-load-plugins.php
+++ includes/.demos/05-load-plugins.php
@@ -1,15 +1,21 @@
 <?php
 
-/* Load plugins inside 'includes/_plugin' directory */
-$dir = Plugin_4_Snippets::DIR . '/_plugins';
+/* Load plugins in '{plugin root}/plugins' directory */
+$dir = Plugin_4_Snippets::DIR . '/plugins';
 $files = scandir( $dir );
 
 foreach( $files as $dirname ) {
 	if ( '.' === $dirname ) continue;
 	if ( '..' === $dirname ) continue;
 
+	/* only loads directories */
 	$path = path_join( $dir, $dirname );
-	if ( ! is_dir( $path ) ) return;
+	if ( ! is_dir( $path ) ) continue;
+
+	/* ignore directorys that starts starts with dot or undescore */
+	$first_char = substr( $dirname, 0, 1 );
+	if ( '_' === $first_char ) continue;
+	if ( '.' === $first_char ) continue;
 
 	$main_files = [
 		"{$dirname}.php",
