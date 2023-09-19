39d9d8963f3c973e2265a5229afe10243433ccdb
Luiz Bills <luizbills@pm.me>
2023-09-19 12:20:51 -0300

    Fix 00-autoload.php

--- includes/.demos/00-autoload.php
+++ includes/.demos/00-autoload.php
@@ -2,7 +2,7 @@
 
 /* Load the Composer autoload */
 $autoload = Plugin_4_Snippets::DIR . '/vendor/autoload.php';
-if ( file_exists( $autoload ) {
+if ( file_exists( $autoload ) ) {
 	include $autoload;
 } else {
 	wp_die( "<code>$autoload</code> not found!" );
